c7fb6257fd2ad99ce34b1bd1e576aa5310108267
Nithin Daniel <66398646+nithindaniel1@users.noreply.github.com>
2022-06-07 10:46:28 +0530

    Comment addded

--- zip.py
+++ zip.py
@@ -5,9 +5,9 @@ entries = os.listdir(input)
 
 for item in entries:
    if item.endswith(".zip"):
-      # filePath = input + "/" + item
+      # filePath = input + "/" + item # For linux
       #  or
-      filePath=os.path.join(input,output)
+      filePath=os.path.join(input,output) # For Windows
       print("Unzipping " + item)
       with zipfile.ZipFile(filePath, 'r') as zip_ref:
          zip_ref.extractall(output)
